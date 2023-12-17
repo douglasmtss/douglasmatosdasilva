04f32d7af52010f6705012a7179e24e37416c90f
douglasmatosdev <douglasmatosdev@gmail.com>
2023-12-17 13:32:54 -0300

    npx create-next-app@latest

--- TODO.md
+++
@@ -1,6 +0,0 @@
-# TODO
-✅️
-⛔️
-
-
-- ⛔️ Implementar layout dos posts
--- project-source-code/src/store.ts
+++
@@ -1,8 +0,0 @@
-import { atom } from "jotai"
-
-export type MenuNav = {
-    isOpen: boolean
-}
-
-
-export const menuNav = atom<MenuNav>({ isOpen: false })
