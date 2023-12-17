d88404b147f7c6ba72aaa81fc8e12c9c26e0ab34
douglasmatosdev <douglasmatosdev@gmail.com>
2023-12-17 20:24:02 -0300

    vercel

--- src/utils/ascii_utf8_binary.ts
+++ src/utils/ascii_utf8_binary.ts
@@ -1,9 +1,10 @@
+/* eslint-disable camelcase */
 export const convert = {
     asciiUtf8ToBinary(text: string): string {
         let binaryString = ''
 
         for (let i = 0; i < text.length; i++) {
-            binaryString += text[i].charCodeAt(0).toString(2) + " ";
+            binaryString += text[i].charCodeAt(0).toString(2) + ' '
         }
 
         return binaryString
@@ -15,7 +16,7 @@ export const convert = {
         if (binaryString.length) {
             binaryString.split(' ').forEach(byte => {
                 if (byte.trim().length) {
-                    ascii_utf8String += String.fromCharCode(parseInt(byte.trim(), 2));
+                    ascii_utf8String += String.fromCharCode(parseInt(byte.trim(), 2))
                 }
             })
         }
@@ -23,4 +24,3 @@ export const convert = {
         return ascii_utf8String
     }
 }
-
