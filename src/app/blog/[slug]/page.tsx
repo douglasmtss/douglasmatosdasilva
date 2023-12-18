25a113ac00c5fa5f1dc09c4275d2811b65537453
douglasmatosdev <douglasmatosdev@gmail.com>
2023-12-18 10:52:14 -0300

    first test reading mdx files and rendering jsx from mdx

---
+++ src/app/blog/[slug]/page.tsx
@@ -0,0 +1,31 @@
+import { Mdx } from '@/components/Mdx'
+import { Doc, allDocs } from 'contentlayer/generated'
+import { notFound } from 'next/navigation'
+
+interface PageProps {
+    params: {
+        slug: string
+    }
+}
+
+async function getDocFromParams(slug: string): Promise<Doc> {
+    const doc = allDocs.find(doc => doc.slugAsParams === slug)
+
+    if (!doc) notFound()
+
+    return doc
+}
+
+export default async function page({ params }: PageProps): Promise<JSX.Element> {
+    const doc = await getDocFromParams(params.slug)
+
+    console.log(doc)
+
+    return (
+        <div>
+            <h1>{doc.title}</h1>
+            {/* <div>{JSON.stringify(doc)}</div> */}
+            <Mdx code={doc.body.code} />
+        </div>
+    )
+}
