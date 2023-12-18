3c8b81b88b229772ef9ef2c6efe5c422dabeb589
douglasmatosdev <douglasmatosdev@gmail.com>
2023-12-17 22:06:39 -0300

    fix toggle theme

--- src/components/ToggleTheme.tsx
+++ src/components/ToggleTheme.tsx
@@ -1,34 +1,22 @@
 'use client'
-import { useState } from 'react'
+import { useToggleSystemOrAppTheme } from '@/hooks/useToggleSystemOrAppTheme'
 import { BsFillMoonStarsFill, BsFillSunFill } from 'react-icons/bs'
 
 export const TogggleTheme = (): JSX.Element => {
-    const [darkMode, setDarkMode] = useState(true)
+    const { theme, toggleTheme } = useToggleSystemOrAppTheme()
 
-    function handleClick(): void {
-        if (!('theme' in localStorage)) {
-            document.documentElement.classList.remove('dark')
-        } else if (localStorage.theme === 'dark' || !('theme' in localStorage)) {
-            document.documentElement.classList.add('dark')
-        } else {
-            document.documentElement.classList.remove('dark')
-        }
-
-        if (localStorage.theme === 'dark') {
-            localStorage.theme = 'light'
-            setDarkMode(true)
-        } else {
-            localStorage.theme = 'dark'
-            setDarkMode(false)
-        }
-    }
+    const isDarkMode = theme === 'dark'
 
     return (
-        <div className="flex items-center" title={`Change theme to ${darkMode ? 'light mode' : 'dark mode'}`}>
-            {darkMode ? (
-                <BsFillSunFill onClick={handleClick} className="z-[999] cursor-pointer text-2xl text-yellow-500" />
+        <div
+            onClick={toggleTheme}
+            className="flex items-center cursor-pointer"
+            title={`Change theme to ${isDarkMode ? 'light mode' : 'dark mode'}`}
+        >
+            {isDarkMode ? (
+                <BsFillSunFill className="z-[999] cursor-pointer text-2xl text-yellow-500" />
             ) : (
-                <BsFillMoonStarsFill onClick={handleClick} className="z-[999] cursor-pointer text-2xl text-blue-800" />
+                <BsFillMoonStarsFill className="z-[999] cursor-pointer text-2xl text-blue-800" />
             )}
         </div>
     )
