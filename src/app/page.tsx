9c35ba1b03380536de05f64cc051abe095e04b9a
douglasmatosdev <douglasmatosdev@gmail.com>
2023-12-24 12:15:43 -0300

    WIP:Google Analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,19 @@
 import WrapperPage from '@/components/WrapperPage'
+import Script from 'next/script'
 
 export default function Home(): JSX.Element {
     return (
         <WrapperPage>
+            <Script src="https://www.googletagmanager.com/gtag/js?id=G-8J47CXLFL2"></Script>
+            <Script id="google-analytics">
+                {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                  
+                    gtag('config', 'G-8J47CXLFL2');
+                `}
+            </Script>
             <header className="flex flex-col items-center py-8 w-max mx-auto">
                 <h1 className="font-ranga mt-8 text-5xl text-dmds-2 dark:text-dmds-1 md:text-8xl tracking-tighter mb-8">
                     Douglas Silva
