c256503d6b5063edb8979e70c9b8eb84a7fc69da
douglasmatosdev <douglasmatosdev@gmail.com>
2023-12-28 18:05:51 -0300

    update home

--- src/components/PostPreview.tsx
+++ src/components/PostPreview.tsx
@@ -1,5 +1,3 @@
-import { useState } from 'react'
-import { motion } from 'framer-motion'
 import readingTime from 'reading-time'
 import Link from 'next/link'
 import stripHtml from '@/lib/strip-html'
@@ -41,98 +39,11 @@ export default function PostPreview(props: PostPreviewProps): JSX.Element {
 }
 
 function Animation(props: { index: string; children: React.ReactNode }): JSX.Element {
-    const [hovered, setHovered] = useState('')
-    const isHovered = hovered === props.index
-
     return (
-        <motion.div
-            onHoverStart={() => setHovered(props.index)}
-            onHoverEnd={() => setHovered('')}
-            className="realative w-full"
-        >
-            {isHovered && (
-                <motion.div
-                    layoutId="featuredArticles"
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    exit={{ opacity: 0 }}
-                    className="absolute top-0 left-0 right-0 bottom-0 z-[-1] rounded-md"
-                />
-            )}
+        <div className="realative w-full">
+            <div className="absolute top-0 left-0 right-0 bottom-0 z-[-1] rounded-md" />
 
             {props.children}
-        </motion.div>
+        </div>
     )
 }
-
-// const Article = styled('a', {
-//     border: '0',
-//     width: '370px',
-//     marginLeft: '20px',
-//     textDecoration: 'none',
-//     '&:hover': { opacity: 1 },
-//     '&:first-child': { marginLeft: 0 }
-// })
-
-// const Container = styled('div', {
-//     display: 'flex',
-//     flexDirection: 'column'
-// })
-
-// const ImageContainer = styled('div', {
-//     borderRadius: '8px',
-//     width: '370px',
-//     height: '180px',
-//     marginBottom: '20px',
-//     backgroundSize: 'cover',
-//     backgroundRepeat: 'no-repeat',
-//     backgroundPosition: 'center center',
-//     filter: 'grayscale(1)'
-// })
-
-// const Content = styled('div', {
-//     maxWidth: '450px',
-//     marginRight: '20px',
-//     '@bp2': { maxWidth: '100%', marginRight: 0 }
-// })
-
-// const Title = styled('h3', {
-//     color: '$primary',
-//     margin: 0
-// })
-
-// const Description = styled('p', {
-//     color: '$secondary',
-//     display: '-webkit-box',
-//     margin: 0,
-//     WebkitLineClamp: '2',
-//     WebkitBoxOrient: 'vertical',
-//     overflow: 'hidden'
-// })
-
-// const Stats = styled('p', {
-//     margin: '5px 0 0',
-//     color: '$primary',
-//     textTransform: 'uppercase',
-//     display: 'inline-block',
-//     fontWeight: 500,
-//     letterSpacing: '1.2px',
-//     fontSize: '12px'
-// })
-
-// const AnimContainer = styled(motion.div, {
-//     position: 'relative',
-//     width: '100%',
-//     padding: '20px'
-// })
-
-// const AnimHovered = styled(motion.div, {
-//     position: 'absolute',
-//     top: '0',
-//     left: '0',
-//     right: '0',
-//     bottom: '0',
-//     background: '$hover',
-//     borderRadius: '$borderRadius',
-//     zIndex: -1
-// })
---
+++ src/utils/info.ts
@@ -0,0 +1,100 @@
+type FieldNameAndUrl = {
+    name: string
+    url: string
+}
+
+interface Info {
+    currentYear: number
+    age: number
+    experienceAge: number
+    collegeSemesters: number
+    inCollegeNow: boolean
+    name: string
+    fullName: string
+    github: FieldNameAndUrl
+    company: {
+        intelie: FieldNameAndUrl
+        viasat: FieldNameAndUrl
+    }
+    college: FieldNameAndUrl
+    socialMedias: {
+        github: FieldNameAndUrl
+        instagram: FieldNameAndUrl
+        facebook: FieldNameAndUrl
+        twitter: FieldNameAndUrl
+        telegram: FieldNameAndUrl
+        linkedin: FieldNameAndUrl
+        discord: FieldNameAndUrl
+    }
+}
+
+export default function info(): Info {
+    const name = 'Douglas Matos'
+    const fullName = name + ' da Silva'
+    const currentYear = +new Date().getFullYear()
+    const age = currentYear - 1993
+    const experienceAge = currentYear - 2019
+    const collegeSemesters = (currentYear - 2020) * 2
+    const inCollegeNow = currentYear <= 2025
+    const github = {
+        name: 'douglasmatosdev',
+        url: 'https://github.com/douglasmatosdev'
+    }
+    const company = {
+        intelie: {
+            name: 'Intelie by ViaSat',
+            url: 'https://www.intelie.ai/'
+        },
+        viasat: {
+            name: 'ViaSat',
+            url: 'https://www.viasat.com/'
+        }
+    }
+
+    const college = {
+        name: 'Estacio',
+        url: 'https://estacio.br'
+    }
+
+    const socialMedias = {
+        github,
+        instagram: {
+            name: '',
+            url: ''
+        },
+        facebook: {
+            name: 'devdouglasmatos',
+            url: 'https://www.facebook.com/devdouglasmatos'
+        },
+        twitter: {
+            name: 'devdouglasmatos',
+            url: 'https://twitter.com/devdouglasmatos'
+        },
+        telegram: {
+            name: '',
+            url: ''
+        },
+        linkedin: {
+            name: 'devdouglasmatos',
+            url: 'www.linkedin.com/in/devdouglasmatos'
+        },
+        discord: {
+            name: '',
+            url: ''
+        }
+    }
+
+    return {
+        currentYear,
+        age,
+        experienceAge,
+        collegeSemesters,
+        inCollegeNow,
+        name,
+        fullName,
+        company,
+        college,
+        github,
+        socialMedias
+    }
+}
