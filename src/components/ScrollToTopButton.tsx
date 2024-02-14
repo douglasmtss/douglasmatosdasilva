86362055bb657f6ab953c403e610ae7d388495aa
douglasmatosdev <douglasmatosdev@gmail.com>
2024-02-14 18:53:57 -0300

    run scroll event only first render

--- src/components/ScrollToTopButton.tsx
+++ src/components/ScrollToTopButton.tsx
@@ -27,7 +27,8 @@ export default function ScrollToTopButton(): JSX.Element {
                 controls.start('hide')
             }
         })
-    })
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
 
     return (
         <motion.button
